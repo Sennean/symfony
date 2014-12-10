dbc4738156450afe36fb8b6bba33e026328caae8
Sennean <michal.kajszczak@gmail.com>
2014-12-10 15:44:30 +0100

    ver 0.1
    Symfony - projekt podstawy

---
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Bookkeeper\ManagerBundle\Controller; 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation
+class BookController extends Controller {
+
+public function indexAction() {
+
+}
+public function showAction($id) {
+
+}
+public function newAction() {
+
+}
+public function createAction( Request $request) {
+
+}
+public function editAction($id) {
+
+}
+
+public function updateAction(Request $request, $id) {
+
+}
+
+
+
+
+
+
+}
+
+
+?>
